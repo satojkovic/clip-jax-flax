3a04c718f4fb55f9f57ef671be40084a9ddef0e3
satojkovic <satojkovic@gmail.com>
2024-01-31 11:43:05 +0900

    Add TextEncoder

--- pytorch/model.py
+++ pytorch/model.py
@@ -1,6 +1,7 @@
 import timm
 import torch
 from torch import nn
+import transformers
 
 
 class ImageEncoder(nn.Module):
@@ -19,3 +20,20 @@ class ImageEncoder(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+class TextEncoder(nn.Module):
+    def __init__(self, model_name: str, trainable: bool = True) -> None:
+        super().__init__()
+        self.model = transformers.AutoModel.from_pretrained(model_name)
+
+        for param in self.model.parameters:
+            param.requires_grad = trainable
+
+        self.target_token_idx = 0
+
+    def forward(self, input_ids, attention_mask):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        last_hidden_state = output.last_hidden_state
+
+        return last_hidden_state[:, self.target_token_idx, :]

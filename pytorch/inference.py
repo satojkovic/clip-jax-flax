f7640b14bab91652c0cd25f079175504009991e8
satojkovic <satojkovic@gmail.com>
2024-04-18 20:54:22 +0900

    Prepare an input image

--- pytorch/inference.py
+++ pytorch/inference.py
@@ -3,6 +3,8 @@ import torch
 from model import CLIPDualEncoderModel
 from datasets import load_dataset
 from transformers import AutoTokenizer
+import albumentations as A
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -35,6 +37,18 @@ if __name__ == '__main__':
         max_length=max_length,
         return_tensors='pt').to(device)
     print(label_tokens['input_ids'][0][:10])
+
+    image = imagenette[0]['image']
+    target_size = 224
+    transforms = A.Compose(
+        [
+            A.Resize(target_size, target_size, always_apply=True),
+            A.Normalize(max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+    image = transforms(image=np.array(image))['image']
+    image = torch.tensor(image).permute(2, 0, 1).float()
+    print(image.shape)
 
     model = CLIPDualEncoderModel.load_from_checkpoint(
         args.ckpt_path,

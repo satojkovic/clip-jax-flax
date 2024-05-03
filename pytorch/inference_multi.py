7ebdee922d6a34c2547c8255d007f15336ae2b1f
satojkovic <satojkovic@gmail.com>
2024-05-03 13:30:53 +0900

    Prompt ensembling

--- pytorch/inference_multi.py
+++ pytorch/inference_multi.py
@@ -7,13 +7,33 @@ import albumentations as A
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
+from imagenet_info import imagenet_classes, imagenet_templates
 
 
-def preproc_image(image, transforms):
+def preproc_image(image, transforms, device):
     image = transforms(image=np.array(image))['image']
     image = torch.tensor(image).permute(2, 0, 1).float().to(device)
     image = image.unsqueeze(0)
     return image
+
+
+def zeroshot_classifier(labels, templates, model, tokenizer, device, max_length=100):
+    zeroshot_weights = []
+    for label in tqdm(labels):
+        texts = [template.format(label) for template in templates]
+        texts = tokenizer(
+            text=texts,
+            padding=True,
+            truncation=True,
+            max_length=max_length,
+            return_tensors='pt').to(device)
+        text_embeddings = model.encode_text(texts['input_ids'], texts['attention_mask'])
+        text_embeddings = text_embeddings.mean(dim=0)
+        text_embeddings /= text_embeddings.norm()
+        text_embeddings = text_embeddings.cpu().detach().numpy()
+        zeroshot_weights.append(text_embeddings)
+    zeroshot_weights = np.stack(zeroshot_weights, axis=1)
+    return zeroshot_weights
 
 
 if __name__ == '__main__':
@@ -21,7 +41,9 @@ if __name__ == '__main__':
     parser.add_argument('--ckpt_path', required=True, help='Path to ckpt.')
     args = parser.parse_args()
 
+    #
     # Zero-shot classification
+    #
 
     imagenette = load_dataset(
         'frgfm/imagenette',
@@ -29,24 +51,12 @@ if __name__ == '__main__':
         split='validation',
         revision='4d512db'
     )
-    # Create labels
-    labels = imagenette.info.features['label'].names
-    clip_labels = [f'a photo of a {label}' for label in labels]
-    max_length = max([len(clip_label) for clip_label in clip_labels])
 
     image_encoder_alias = "resnet50"
     text_encoder_alias = "distilbert-base-uncased"
 
     device = 'mps'
     tokenizer = AutoTokenizer.from_pretrained(text_encoder_alias)
-    label_tokens = tokenizer(
-        text=clip_labels,
-        padding=True,
-        truncation=True,
-        max_length=max_length,
-        return_tensors='pt').to(device)
-    print(f'label tokens: {label_tokens.keys()}')
-
     model = CLIPDualEncoderModel.load_from_checkpoint(
         args.ckpt_path,
         image_encoder_alias=image_encoder_alias,
@@ -61,17 +71,18 @@ if __name__ == '__main__':
         ]
     )
 
-    inputs = {key: value for key, value in label_tokens.items()}
+    # Prompt ensembling
+    labels = imagenette.info.features['label'].names
+    zeroshot_weights = zeroshot_classifier(labels, imagenet_templates, model, tokenizer, device)
+
     preds = {}
     for i in tqdm(range(len(imagenette))):
         if imagenette[i]['image'].mode != 'RGB':
             continue
-        image = preproc_image(imagenette[i]['image'], transforms=transforms)
-        inputs['image'] = image
-        image_embeddings, text_embeddings = model(inputs)
+        image = preproc_image(imagenette[i]['image'], transforms=transforms, device=device)
+        image_embeddings = model.encode_image(image)
         image_embeddings = image_embeddings.cpu().detach().numpy()
-        text_embeddings = text_embeddings.cpu().detach().numpy()
-        score = np.dot(image_embeddings, text_embeddings.T)
+        score = np.dot(image_embeddings, zeroshot_weights)
         pred = np.argmax(score)
         preds[i] = pred
 
